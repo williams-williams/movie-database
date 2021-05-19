b30251a8cea69006fec73a0a6f804dff4e699403
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-05-19 11:10:51 -0500

    delete buttons

--- js/movie-app.js
+++ js/movie-app.js
@@ -16,18 +16,39 @@ const getMovies = () => {
       for (let movie of movies) {
         htmlStr += `<div class="card" style="width: 18rem;">
     <img src="${movie.poster}" class="card-img-top" alt="...">
+    <button class="btn btn-danger testD" data-value="${movie.id.toString()}">Remove Movie</button>
     <div class="card-body">
       <h5 class="card-title">${movie.title}</h5>
       <p class="card-text">${movie.plot}</p>
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item">${movie.rating} out of 5</li>
-      <li class="list-group-item">Release year: ${movie.year}</li>
-      <li class="list-group-item">Directed by: ${movie.director}</li>
+
+     <li class="list-group-item">${movie.rating} out of 5</li>
+     <li class="list-group-item">Release year: ${movie.year}</li>
+     <li class="list-group-item">Directed by: ${movie.director}</li>
     </ul>
   </div>`
       }
-      $("#container").html(htmlStr)
+      $("#container").html(htmlStr);
+
+    })
+    .then(function(){
+      //Delete event needs to be inside so it happens after the
+      $(".testD").click(function () {
+        console.log("button clicked");
+        var idTag = $(this).attr("data-value");
+        console.log(idTag);
+        let deleteMovie = {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        };
+
+  let inputVal = $('#movie-id-delete').val();
+  fetch(`https://alkaline-aluminum-bulb.glitch.me/movies/${idTag}`, deleteMovie)
+    .then(getMovies)
+      });
     })
 }
 getMovies();
@@ -42,75 +63,68 @@ $('#newMovieButton').click(() => {
     "rating": $('#newMovieRating').val(),
 
   };
-  function testMovie(){
+
+  function testMovie() {
     let result;
 
-  movieData.forEach(function(movieE,indexE){
-    if(movieE.title === newMovie.title){
-      // alert(`${newMovie.title} is already on the list.`)
-      result = false
-    }
-  });
-  return (result === undefined)
+    movieData.forEach(function (movieE, indexE) {
+      if (movieE.title === newMovie.title) {
+        // alert(`${newMovie.title} is already on the list.`)
+        result = false
+      }
+    });
+    return (result === undefined)
   }
+
   console.log(testMovie())
 
-    let postNewMovie = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newMovie)
-    };
+  let postNewMovie = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newMovie)
+  };
 
-    fetch("https://alkaline-aluminum-bulb.glitch.me/movies")
-      .then(resp => resp.json())
-      .then(movies => {
-        // for (let movie of movies) {
-          if (testMovie()) {
-            fetch("https://alkaline-aluminum-bulb.glitch.me/movies",)
-              .then(getMovies)
-              .then(console.log(movies))
-          } else {
-            alert("hey, that movie already exists!");
-            // break;
-          }
-        // }
-      })
+  fetch("https://alkaline-aluminum-bulb.glitch.me/movies")
+    .then(resp => resp.json())
+    .then(movies => {
+      // for (let movie of movies) {
+      if (testMovie()) {
+        fetch("https://alkaline-aluminum-bulb.glitch.me/movies", postNewMovie)
+          .then(getMovies)
+          .then(console.log(movies))
+      } else {
+        alert("hey, that movie already exists!");
+        // break;
+      }
+      // }
+    })
 })
 
 
 
-// DELETE movie / Be careful of what is deleted
-let deleteMovie = {
-    method: 'DELETE',
-    headers: {
-        'Content-Type': 'application/json',
-    }
-};
+// EDIT movie / each change needs drop down options
+$("#editMovieButton").click(function(){
+  let editThis = {
+    "title": $('#editTitle').val(),
+    "plot": $('#editPlot').val(),
+    "rating": $('#editRating').val(),
+    "year": $('#editYear').val(),
+    "director": $('#editDirector').val(),
+  }
 
-$("#deleteMovieButton").click(() => {
-    let inputVal = $('#movie-id-delete').val();
-    fetch(`https://alkaline-aluminum-bulb.glitch.me/movies/${inputVal}`, deleteMovie)
-        .then(getMovies)
+  let editOptions = {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(editThis)
+  };
+
+  let editMovieinputVal = $('#movie-id-edit').val();
+  fetch(`https://alkaline-aluminum-bulb.glitch.me/movies/${editMovieinputVal}`, editOptions).then(getMovies);
+
 });
 
-// EDIT movie / each change needs drop down options
-// let editThis = {
-//     "title": $('#editTitle').innerText,
-//     "plot": $('#editPlot').innerText,
-//     "rating": $('#editRating').innerText,
-//     "year": $('#editYear').innerText,
-//     "director": $('#editDirector').innerText,
-// }
 
-// let editOptions = {
-//     method: 'PATCH',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(editThis)
-// };
-//
-// let editMovieinputVal = $('#movie-id-edit').val();
-// fetch(`https://alkaline-aluminum-bulb.glitch.me/movies/${editMovieinputVal}`, editOptions).then(getMovies);
